e880972f63e79fbb3ad15a47dc2b01ab05fb2b0d
armand <32680084+armanddd@users.noreply.github.com>
2021-09-02 02:56:15 +0200

    Create elder_age.py

---
+++ elder_age.py
@@ -0,0 +1,9 @@
+# def elder_age(m, n, l, t):
+#     time = 0
+#
+#     for index in range(0, m):
+#         for index2 in range(0, n):
+#             if (index ^ index2) - l > 0:
+#                 time += (index ^ index2) - l
+#
+#     return time % t
