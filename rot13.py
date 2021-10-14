9748ae2b1122584f13277020e5b037dd4c54f81d
armand <32680084+armanddd@users.noreply.github.com>
2021-10-14 15:23:01 +0200

    Update rot13.py

--- rot13.py
+++ rot13.py
@@ -3,14 +3,13 @@ def rot13(message):
     for c in message:
         if ord(c) > 64 and ord(c) < 91:
             if 90 % ord(c) > 13:
-                res = ord(c) + 13
+                c = chr(ord(c) + 13)
             else:
-                res = 64 + (13 - (90 - ord(c)))
-            ans += chr(res)
+                c = chr(64 + (13 - (90 - ord(c))))
         if ord(c) > 96 and ord(c) < 123:
             if 122 % ord(c) > 13:
-                res = ord(c) + 13
+                c = chr(ord(c) + 13)
             else:
-                res = 96 + (13 - (122 - ord(c)))
-            ans += chr(res)
+                c = chr(96 + (13 - (122 - ord(c))))
+        ans += c
     return ans
