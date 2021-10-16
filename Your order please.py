fe8b6654e9527e13fcddc9dc4e1742368e787c10
armand <32680084+armanddd@users.noreply.github.com>
2021-10-16 22:41:08 +0200

    Update Your order please.py

--- Your order please.py
+++ Your order please.py
@@ -5,6 +5,6 @@ def order(sentence):
         return ""
     indexes = list(map(lambda x: int(x) - 1, re.findall(r'\d+', sentence)))
     ans = []
-    for id,val in enumerate(sentence.split(" ")):
+    for id in range(len(sentence.split(" "))):
         ans.append(sentence.split(" ")[indexes.index(id)])
     return " ".join(ans)
